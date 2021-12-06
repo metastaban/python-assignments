2caf6a4f793eb302dda795e6e14899c00b8290ed
memin <mehmetemin@tastaban.net>
2021-12-06 10:35:32 +0300

    Assignment 10.07 done

---
+++ assignment-10.07-fizzbuzz_numbers.py
@@ -0,0 +1,16 @@
+# Task : Print the Fizz Buzz numbers.
+# Print numbers from 1 to 100 inclusively following these instructions:
+# if a number is multiple of 3, print "Fizz" instead of this number,
+# if a number is multiple of 5, print "Buzz" instead of this number,
+# for numbers that are multiples of both 3 and 5, print "FizzBuzz",
+# print the rest of the numbers unchanged.
+
+for i in range(1,101):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
